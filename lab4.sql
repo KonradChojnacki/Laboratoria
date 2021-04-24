3e07a21517883b0708ec30b96e72d193c2929f4d
Konrad Chojnacki <45129336+KonradChojnacki@users.noreply.github.com>
2021-04-24 14:59:01 +0200

    Add files via upload

--- lab4.sql
+++ lab4.sql
@@ -17,16 +17,19 @@ sylabus bytea
 CREATE TABLE subjects_students(
 pesel varchar(11),
 id_subject varchar(4),
-grade varchar(1),
+grade real,
+semester varchar(1),
 year varchar(4),
 FOREIGN KEY (pesel) REFERENCES students(pesel),
 FOREIGN KEY (id_subject) REFERENCES subjects(id_subject)
 );
 
 CREATE TABLE old_grades(
-pesel varchar(9),
+pesel varchar(11),
 id_subject varchar(4),
-old_grade varchar(1),
+old_grade real,
+new_grade real,
+semester varchar(1),
 year varchar(4),
 FOREIGN KEY (pesel) REFERENCES students(pesel),
 FOREIGN KEY (id_subject) REFERENCES subjects(id_subject)
@@ -41,7 +44,7 @@ $BODY$
 BEGIN
 	IF NEW.grade <> OLD.grade THEN
 		 INSERT INTO old_grades
-		 VALUES(old.pesel,old.id_subject,old.grade,old.year);
+		 VALUES(old.pesel,old.id_subject,old.grade,new.grade,old.semester,old.year);
 	END IF;
 	RETURN NEW;
 END;
@@ -53,3 +56,13 @@ CREATE TRIGGER subjects_students_UPDATE
   ON subjects_students
   FOR EACH ROW
   EXECUTE PROCEDURE subjects_students_UPDATE()
+  
+  /* studenci z srednia powyzej 4.5 */
+SELECT first_name,last_name, sum(grade)/count(grade)AS srednia 
+FROM subjects_students ss JOIN students s ON ss.pesel=s.pesel
+GROUP BY first_name,last_name
+HAVING sum(grade)/count(grade)>= 4.5
+ORDER BY sum(grade) desc;
+/*Studenci ktorzy nie zaliczyli ostatniego semestru */
+SELECT first_name,last_name FROM subjects_students ss JOIN students s ON ss.pesel=s.pesel
+WHERE semester=(SELECT MAX(semester) FROM subjects_students) AND grade=2
